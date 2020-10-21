005a5f0f371b96d1f6568f7b0279f568a8eed2d0
slightlyevil <60799804+slightlyevil@users.noreply.github.com>
2020-10-22 06:19:46 +0800

    Update pdu_create_iad1.py

--- pdu_create_iad1.py
+++ pdu_create_iad1.py
@@ -11,3 +11,6 @@ currentMonth = date(day=dt.day, month=dt.month, year=dt.year).strftime('%B %Y')
 #print(currentMonth)
 
 # upload file from `/some/path/attachment.txt`
+
+a = "https://global.sitesafety.trendmicro.com/result.php"
+b = "https://registry.npmjs.org/@trendmicro/react-portal/-/react-portal-1.0.1.tgz"
